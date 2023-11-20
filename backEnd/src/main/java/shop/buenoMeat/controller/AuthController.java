3304f3f3fefe57702e667bbf93104add493c9837
unknown <kdw981129@gmail.com>
2023-11-20 21:26:12 +0900

    프론트와 백엔드 포트 연결 및 회원가입
    proxy 설정으로 cors 오류 해결 및 json데이터 정상적으로 넘어오는지 확인

---
+++ backEnd/src/main/java/shop/buenoMeat/controller/AuthController.java
@@ -0,0 +1,21 @@
+package shop.buenoMeat.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import shop.buenoMeat.dto.MemberDto;
+import shop.buenoMeat.service.MemberService;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/auth/join")
+    public ResponseEntity<String> join(@RequestBody MemberDto memberDto) {
+        memberService.join(memberDto.toEntity());
+        return ResponseEntity.ok("회원가입이 성공적으로 완료됐습니다!");
+    }
+
+}
