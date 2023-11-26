62cebe460be92a0afd6636e009a3d7343c4d7c72
unknown <kdw981129@gmail.com>
2023-11-26 23:21:57 +0900

    마이페이지 및 비밀번호 수정
    [rename] 재검토후 다시 올림

--- backEnd/src/main/java/shop/buenoMeat/dto/LoginDto.java
+++ backEnd/src/main/java/shop/buenoMeat/dto/LoginDto.java
@@ -1,12 +1,26 @@
 package shop.buenoMeat.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class LoginDto {
 
-    private String email;
-    private String pw;
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class loginRequestDto {
+        private String email;
+        private String pw;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class loginResponseDto {
+        private String msg;
+        private String nickname;
+        private Long id;
+    }
 }
