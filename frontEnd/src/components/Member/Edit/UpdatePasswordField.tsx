adc35155671ca2c44fad4d10e974550f22b49334
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2023-11-27 14:17:12 +0900

    [ISSUE] pr merge 오류 해결

--- frontEnd/src/components/Member/Edit/UpdatePasswordField.tsx
+++ frontEnd/src/components/Member/Edit/UpdatePasswordField.tsx
@@ -34,7 +34,7 @@ const UpdatePasswordField = ({
             alert("신규 비밀번호와 재입력 비밀번호가 같지 않습니다");
             setCheckedPw('');
             setValue("newPw", '');
-            // post axios 코드
+            
         }
         
         axios.patch(`/mypage/${userId}/password`, data)
