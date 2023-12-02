c2ce11f7a2b69ecd748a174e39e60013510e94b6
Taeil Shin <106367655+bigone-77@users.noreply.github.com>
2023-12-02 15:04:18 +0900

    .

--- backEnd/src/main/frontEnd/src/api/requests.ts
+++
@@ -1,9 +0,0 @@
-const requests = {
-    hotBueno: "http://localhost:8080/products/hot",
-    newBueno: "products/new",
-    saleBueno: "products/sale",
-    login: "/auth/login",
-    join: "/auth/join",
-}
-
-export default requests;
